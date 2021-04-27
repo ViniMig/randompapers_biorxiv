fbf34cc433becb94a92e1b0a641feb0bc7593fff
ViniMig <vinicio.m14@gmail.com>
2021-04-27 12:37:03 +0100

    update nb and add some style to text

--- src/rand_paps.py
+++ src/rand_paps.py
@@ -52,6 +52,7 @@ docs = out["collection"]
 choice_count = 1
 for i in choice:
 	doc = docs[i]
+	fp.write("<h4>Category: "+str(doc["category"])+" </h4>")
 	authors = list(doc["authors"].split(";"))
     
 	num_authors = len(authors)
@@ -63,7 +64,6 @@ for i in choice:
 		name2 = str(authors[1])
 		fp.write(' &amp; ' + name2.split(',')[0])
 	
-	fp.write(", Category: "+str(doc["category"]))
 	fp.write(', '+str(doc["date"]))
 	fp.write(", doi: "+str(doc["doi"]))
 	fp.write(", title: <a href=https://www.biorxiv.org/content/"+str(doc["doi"])+"v"+str(doc["version"])+">"+str(doc["title"])+"</a><br>")    
@@ -75,26 +75,26 @@ for i in choice:
 	choice_count+=1
 
 # summary information
-fp.write("<P>Selected on %s, %d %s from a total of %d papers published last month in Biorxiv</p>" % (today.strftime("%A"),today.day,today.strftime("%B %Y"),num_papers))
+fp.write("<br><p>Selected on %s, %d %s from a total of %d papers published last week in Biorxiv</p>" % (today.strftime("%A"),today.day,today.strftime("%B %Y"),num_papers))
+
+# make a list of previous random papers
+# fp.write("<h4>Previous random papers</h4>")
+
+# file_list = glob.glob(www_dir+'*.html')
+# file_list = [os.path.basename(file) for file in file_list]
+# file_list.sort(reverse=True)
+# for fname in file_list:
+# 	if fname!='index.html':
+# 		fp.write('<a href="'+fname+'">'+fname.replace("_",".")[:-5]+'</a> ')
 
 # About
 fp.write("<h4>This uses an adaptation of the code used for Random Papers, please refer to the code at the bottom</h4>")
-fp.write("<h4>About Random Papers</h4>")
-fp.write('''<p>We meet every second Monday at noon at the <a href="http://msi.mcgill.ca/">McGill Space Institute</a> to discuss 5 random astrophysics papers.</p>
-<p>The goal of Random Papers is to gain a broad view of current astrophysics research. For each meeting, we run a script to choose 5 random papers published in the last month in refereed astrophysics journals. This gives a different slice of the literature than the typical astro-ph discussion, with papers from outside our own research areas or those that might not otherwise be chosen for discussion.</p>
-<p>Rather than reading each paper in depth, the goal is to focus on the big picture, with questions such as: How would we summarize the paper in a few sentences? What are the key figures in the paper? What analysis methods are used? Why is this paper being written, and Why now?
+fp.write("<h3>About Random Papers</h3>")
+fp.write('''<p style="font-size:18px;font-style:italic;">We meet every second Monday at noon at the <a href="http://msi.mcgill.ca/">McGill Space Institute</a> to discuss 5 random astrophysics papers.</p>
+<p style="font-size:18px;font-style:italic;">The goal of Random Papers is to gain a broad view of current astrophysics research. For each meeting, we run a script to choose 5 random papers published in the last month in refereed astrophysics journals. This gives a different slice of the literature than the typical astro-ph discussion, with papers from outside our own research areas or those that might not otherwise be chosen for discussion.</p>
+<p style="font-size:18px;font-style:italic;">Rather than reading each paper in depth, the goal is to focus on the big picture, with questions such as: How would we summarize the paper in a few sentences? What are the key figures in the paper? What analysis methods are used? Why is this paper being written, and Why now?
 </p>
 ''')
-
-# make a list of previous random papers
-fp.write("<h4>Previous random papers</h4>")
-
-file_list = glob.glob(www_dir+'*.html')
-file_list = [os.path.basename(file) for file in file_list]
-file_list.sort(reverse=True)
-for fname in file_list:
-	if fname!='index.html':
-		fp.write('<a href="'+fname+'">'+fname.replace("_",".")[:-5]+'</a> ')
 
 fp.write('''<p><br>
 Image credit: <a href="https://www.nasa.gov/image-feature/celestial-fireworks">NASA/HST</a><br>
